33888d8e1f92d567b8c7f86666bf5e99e580e766
Antoine Drouault <antoinedrouault@hotmail.com>
2022-04-21 22:12:32 +0200

    Add function to place pawns on board

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 const board = document.getElementById("board");
 
+function indexInClass(collection, node) {
+  for (let i = 0; i < collection.length; i++) {
+    if (collection[i] === node) return i;
+  }
+  return -1;
+}
+
+class Pawn {
+  constructor(element) {
+    this.element = element;
+  }
+  index() {
+    return indexInClass(document.getElementsByClassName("pawns"), this.element);
+  }
+  resetPosition() {
+    const squares = document.getElementsByClassName("square");
+    const squareToMoveTo =
+      this.index() < 8
+        ? squares[this.index() + 8]
+        : squares[squares.length - 1 - this.index()];
+    squareToMoveTo.append(this.element);
+  }
+}
+
 function createBoard() {
   const numberOfSquare = 64;
   for (let i = 0; i < numberOfSquare; i++) {
@@ -13,18 +37,18 @@ createBoard();
 
 function createPieces() {
   const pieces = [
-    { name: "black-bishop", img: "./assets/black_bishop.svg", number: 2 },
-    { name: "black-king", img: "./assets/black_king.svg", number: 1 },
-    { name: "black-knight", img: "./assets/black_knight.svg", number: 2 },
-    { name: "black-pawn", img: "./assets/black_pawn.svg", number: 8 },
-    { name: "black-queen", img: "./assets/black_queen.svg", number: 1 },
-    { name: "black-rook", img: "./assets/black_rook.svg", number: 2 },
-    { name: "white-bishop", img: "./assets/white_bishop.svg", number: 2 },
-    { name: "white-king", img: "./assets/white_king.svg", number: 1 },
-    { name: "white-knight", img: "./assets/white_knight.svg", number: 2 },
-    { name: "white-pawn", img: "./assets/white_pawn.svg", number: 8 },
-    { name: "white-queen", img: "./assets/white_queen.svg", number: 1 },
-    { name: "white-rook", img: "./assets/white_rook.svg", number: 2 },
+    { name: "bishops", img: "./assets/black_bishop.svg", number: 2 },
+    { name: "kings", img: "./assets/black_king.svg", number: 1 },
+    { name: "knights", img: "./assets/black_knight.svg", number: 2 },
+    { name: "pawns", img: "./assets/black_pawn.svg", number: 8 },
+    { name: "queens", img: "./assets/black_queen.svg", number: 1 },
+    { name: "rooks", img: "./assets/black_rook.svg", number: 2 },
+    { name: "bishops", img: "./assets/white_bishop.svg", number: 2 },
+    { name: "kings", img: "./assets/white_king.svg", number: 1 },
+    { name: "knights", img: "./assets/white_knight.svg", number: 2 },
+    { name: "pawns", img: "./assets/white_pawn.svg", number: 8 },
+    { name: "queens", img: "./assets/white_queen.svg", number: 1 },
+    { name: "rooks", img: "./assets/white_rook.svg", number: 2 },
   ];
 
   pieces.forEach((piece) => {
@@ -40,3 +64,15 @@ function createPieces() {
 }
 
 createPieces();
+
+let pawns = [];
+
+function placePieces() {
+  const pawnsElements = document.getElementsByClassName("pawns");
+  for (let i = 0; i < pawnsElements.length; i++) {
+    pawns.push(new Pawn(pawnsElements[i]));
+  }
+  pawns.forEach((pawn) => pawn.resetPosition());
+}
+
+placePieces();
